a8ca1f034bb6f9603102619b6803d5d49762c3b0
szymon.m.olejniczak <szymon.m.olejniczak@student.put.poznan.pl>
2025-05-24 16:47:36 +0200

    Dodaj pliki projektów.

---
+++ zad109/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+namespace zad109
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, string> fastaSequences = new Dictionary<string, string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Wyświetlenie okna dialogowego wyboru pliku CSV
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Pliki fasta (*.fasta)|*.fasta|Wszystkie pliki (*.*)|*.*";
+            openFileDialog1.Title = "Wybierz plik CSV do wczytania";
+            openFileDialog1.ShowDialog();
+            // Jeśli użytkownik wybierze plik i zatwierdzi, wczytaj dane z pliku CSV
+            if (openFileDialog1.FileName != "")
+            {
+
+                string currentHeader = null;
+                using (StreamReader reader = new StreamReader(openFileDialog1.FileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.StartsWith(">"))
+                        {
+                            currentHeader = line.Substring(1).Trim();
+                            if (!fastaSequences.ContainsKey(currentHeader))
+                            {
+                                fastaSequences[currentHeader] = "";
+                            }
+                        }
+                        else if (currentHeader != null)
+                        {
+                            fastaSequences[currentHeader] += line.Trim();
+                        }
+                    }
+                }
+            }
+            analize();
+
+
+        }
+        List<Dictionary<string, string>> sekwencjeidane = new List<Dictionary<string, string>>();
+        void analize()
+        {
+            int i = 0;
+            foreach (var entry in fastaSequences)
+            {
+                sekwencjeidane.Add(new Dictionary<string, string>());
+                sekwencjeidane[i]["nazwa"] = entry.Key;
+                sekwencjeidane[i]["wartosc"] = entry.Value;
+
+                int[] tab = new int[5];
+                for (int j = 0; j < entry.Value.Length; j++)
+                {
+                    if (entry.Value.ToUpper()[j] == 'A') tab[0]++;
+                    else if (entry.Value.ToUpper()[j] == 'T') tab[1]++;
+                    else if (entry.Value.ToUpper()[j] == 'G') tab[2]++;
+                    else if (entry.Value.ToUpper()[j] == 'C') tab[3]++;
+                    else tab[4]++;
+                }
+                
+                
+                float var = (((float)(tab[2]) + (float)tab[3])) / (float)entry.Value.Length;
+                sekwencjeidane[i]["gc"] = var.ToString();
+                
+                sekwencjeidane[i]["kondony"] = (entry.Value.Length / 3).ToString();
+                sekwencjeidane[i]["A"] = tab[0].ToString();
+                sekwencjeidane[i]["T"] = tab[1].ToString();
+                sekwencjeidane[i]["G"] = tab[2].ToString();
+                sekwencjeidane[i]["C"] = tab[3].ToString();
+                sekwencjeidane[i]["N"] = tab[4].ToString();
+                System.Windows.Forms.Label mylab = new System.Windows.Forms.Label();
+                mylab.Text = sekwencjeidane[i]["nazwa"];
+                mylab.Location = new Point(10, 10+i*20);
+                mylab.AutoSize = true;
+                this.Controls.Add(mylab);
+                mylab.Click += klik;
+                i++;
+            }
+        }
+
+        private void klik(object sender, System.EventArgs e)
+        {
+
+            System.Windows.Forms.Label temp = (System.Windows.Forms.Label)sender;
+            for (int i = 0; i < sekwencjeidane.Count; i++)
+            {
+                if (sekwencjeidane[i]["nazwa"] == temp.Text)
+                {
+                    //Form2 test = new Form2();
+                    Form2 test = new Form2(sekwencjeidane[i]);
+                    test.Show();
+                }
+            }
+            
+            
+          
+        }
+
+
+
+        private void ExportToCSV( string filePath)
+        {
+            // Tworzenie nagłówka pliku CSV
+            string csvContent = "nazwa,wartosc,gc,kondony,A,T,G,C,N" + Environment.NewLine;
+            // Dodawanie danych z DataGridView
+            foreach (Dictionary<string, string> row in sekwencjeidane)
+            {
+              
+                    csvContent += string.Join(",", Array.ConvertAll(row.ToArray(), c => c.Value)) + Environment.NewLine;
+               
+               
+            }
+            // Zapisanie zawartości do pliku CSV
+            File.WriteAllText(filePath, csvContent);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Pliki CSV (*.csv)|*.csv|Wszystkie pliki (*.*)|*.*";
+            saveFileDialog1.Title = "Wybierz lokalizację zapisu pliku CSV";
+            saveFileDialog1.ShowDialog();
+            // Jeśli użytkownik wybierze lokalizację i zatwierdzi, zapisz plik CSV
+            if (saveFileDialog1.FileName != "")
+            {
+                // Użyj metody ExportToCSV i podaj obiekt DataGridView oraz ścieżkę do pliku CSV
+                ExportToCSV( saveFileDialog1.FileName);
+            }
+        }
+
+        
+    }
+}
---
+++ zad109/Form2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace zad109
+{
+    public partial class Form2 : Form
+    {
+        string name = "myData";
+        List<string> x = new List<string>();
+        List<double> y = new List<double>();
+        public Form2()
+        {
+            InitializeComponent();
+            string[] charttype = new string[] { "Area", "Bar", "Bubble", "Column", "Doughnut", "Line", "Point", "Funnel",
+        "RangeBar", "Spline", "SplineArea", "StepLine" };
+           
+            for (int i = 0; i < 21; i++)
+            {
+                x.Add(i.ToString());
+                y.Add(Math.Pow((i - 10), 2));
+            }
+        }
+        List<string> labelX = new List<string>();
+        public Form2(Dictionary<string, string> temp)
+        {
+            InitializeComponent();
+            string[] charttype = new string[] { "Area", "Bar", "Bubble", "Column", "Doughnut", "Line", "Point", "Funnel",
+        "RangeBar", "Spline", "SplineArea", "StepLine" };
+         
+           
+            int i = 0;
+            int j = 0;
+            name = temp["nazwa"];
+            label1.MaximumSize = new Size(400, 0);
+            label1.AutoSize = true;
+            foreach (var entry in temp)
+            {
+                label1.Text += entry.Key + " " + entry.Value + "\n";
+                if (i > 3)
+                {
+                    //x.Add(j);
+                    x.Add(entry.Key);
+                    labelX.Add(entry.Key);
+                    y.Add(Int32.Parse(entry.Value));
+                    j++;
+                }
+                i++;
+            }
+            rys();
+
+
+
+        }
+
+        void rys()
+        {
+            chart1.BackColor = Color.LightBlue;
+            chart1.Series.Add(name);
+            chart1.Series[name].Color = Color.DarkSlateGray;
+            // chart1.Series[name].Label = "Y = #"+Y_Label+"\nX = #"+X_Label;
+            // chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            // chart1.Series[name].Label = "Y = #" + Y_Label + "\nX = #" + X_Label;
+            chart1.ChartAreas[0].AxisX.TitleForeColor = Color.Gold;
+
+            chart1.Series[name].Points.DataBindXY(x, y);
+            chart1.Series[name].ChartArea = "ChartArea1";
+            chart1.Series[name].SetDefault(true);
+            chart1.Series[name].Enabled = true;
+            chart1.Visible = true;
+            chart1.Show();
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
